bf2197e3a246b45bdf04641cfeef12baa8996ada
Maurício Moreira Soares <MauricioMoreira.Soares@skatteetaten.no>
2026-02-01 12:33:04 +0100

    refactoring: move FEATURES and STATE to config.py

--- trajpy_ui/ui.py
+++ trajpy_ui/ui.py
@@ -1,33 +1,9 @@
-
 import trajpy
 from nicegui import ui
 
 from trajpy_ui.backend import compute_selected, handle_upload, plot_trajectories, save_results, show_about
+from trajpy_ui.config import FEATURES, STATE
 from trajpy_ui.utils import find_free_port
-
-FEATURES = [
-    "Anomalous Exponent",
-    "MSD Ratio",
-    "Fractal dimension",
-    "Anisotropy & Kurtosis",
-    "Straightness",
-    "Velocity description",
-    "Frequency spectrum",
-    "Efficiency",
-    "Gaussianity",
-    "Diffusivity",
-    "Confinement Prob.",
-]
-
-
-state = {
-    "uploaded_files": [],  # liste av (filename, bytes)
-    "trajectories": [],  # liste av tj.Trajectory-objekter
-    "selected_features": set(),
-    "results": {},
-    "last_saved_path": None,
-}
-
 
 ui.markdown(f"# TrajPy GUI — version {trajpy.__version__}")
 
@@ -35,7 +11,7 @@ with ui.row():
     upload = ui.upload(
         label="Upload one or several files (CSV or YAML)",
         multiple=True,
-        on_upload=lambda event: handle_upload(event, state, result_box),
+        on_upload=lambda event: handle_upload(event, STATE, result_box),
     )
 
 # Create a row to place features and plot side by side
@@ -56,9 +32,9 @@ with ui.row():
     about_btn = ui.button("About", on_click=lambda: show_about())
 
 with ui.row():
-    compute_btn = ui.button("Compute!", on_click=lambda: compute_selected(state, checkboxes, result_box, save_btn))
-    plot_btn = ui.button("Plot Trajectories", on_click=lambda: plot_trajectories(state, plot_container, result_box))
-    save_btn = ui.button("Save results (CSV)", on_click=lambda: save_results(state, result_box)).props("disabled")
+    compute_btn = ui.button("Compute!", on_click=lambda: compute_selected(STATE, checkboxes, result_box, save_btn))
+    plot_btn = ui.button("Plot Trajectories", on_click=lambda: plot_trajectories(STATE, plot_container, result_box))
+    save_btn = ui.button("Save results (CSV)", on_click=lambda: save_results(STATE, result_box)).props("disabled")
     result_box = ui.label("No results yet")
 
 
